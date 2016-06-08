4bab7ae9cda64744990262db41a91d3b15010c56
Joel Delight and Terri Bradley <joel@>
2016-06-08 11:04:03 -0700

    create a while loop to provide desired function for words that start with multiple consonants.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-var vowels = ["a", "e", "i", "o", "u"]
+var vowels = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 var pigtranslator = function(input) {
-  return input.concat("ay")
+  var string = input
+
+while (consonants.includes(string.charAt(0))) {
+  string =  string.slice(1) + string.slice(0,1)
+}
+  string += "ay"
+  return string
+
+
 }
