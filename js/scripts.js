6f8cdcbb9929787f5c9870016c9fe8e0e5414ae2
Joel Delight and Terri Bradley <joel@>
2016-06-08 11:37:59 -0700

    add an if statement to alert the user of illegal characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 var vowels = ["a", "e", "i", "o", "u"];
 var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var illegalCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "|", "\\", "/", "~", "[", "]", "{", "}"];
 var pigtranslator = function(input) {
   var string = input
+  var illegalInput = false
+  for (i=0 ; i<string.length ; i ++) {
+      if (illegalCharacters.includes(string.charAt(i))) {
+        illegalInput = true
+      }
+  }
 
-  if (string.charAt(0)=== "q" && string.charAt(1) === "u") {
+  if (illegalInput) {
+    alert("bad input")
+  }
+  else if (string.charAt(0)=== "q" && string.charAt(1) === "u") {
     string = string.slice(2) + string.slice(0,2)
   }
   else { while (consonants.includes(string.charAt(0))) {
