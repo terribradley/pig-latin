ea0add37d40ae978ae7f5bffe2bb33c2b0ba4e83
Joel Delight and Terri Bradley <terri@>
2016-06-08 11:10:50 -0700

    add if logic to accommodate qu words

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@ var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q
 var pigtranslator = function(input) {
   var string = input
 
-while (consonants.includes(string.charAt(0))) {
-  string =  string.slice(1) + string.slice(0,1)
-}
+  if (string.charAt(0)=== "q" && string.charAt(1) === "u") {
+    string = string.slice(2) + string.slice(0,2)
+  }
+  else { while (consonants.includes(string.charAt(0))) {
+    string =  string.slice(1) + string.slice(0,1)
+  }}
   string += "ay"
   return string
-
-
 }
